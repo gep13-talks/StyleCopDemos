092607ed372a888f337641e9cbf3e3ede6409015
gep13 <gep13@gep13.co.uk>
2012-05-21 20:08:18 +0100

    Added the basic project template for Demo 3

---
+++ src/Demo 3 - StyleCop As Custom Task/Demo 3 - StyleCop As Custom Task/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo_3___StyleCop_As_Custom_Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Demo 3 - StyleCop As Custom Task/Demo 3 - StyleCop As Custom Task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Demo_3___StyleCop_As_Custom_Task
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
