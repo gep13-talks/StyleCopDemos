910d24f792be1207626cece9423b5a5043d4879d
gep13 <gep13@gep13.co.uk>
2012-05-21 21:23:58 +0100

    Added new project for Demo 4 with no StyleCop errors.

---
+++ src/Demo 4 - StyleCop Compliance/Demo 4 - StyleCop Compliance/Form1.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="gep13">
+//   Copyright (c) gep13 2012
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Demo_4___StyleCop_Compliance
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The form 1.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Demo 4 - StyleCop Compliance/Demo 4 - StyleCop Compliance/Program.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="gep13">
+//   Copyright (c) gep13 2012
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Demo_4___StyleCop_Compliance
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal static class Program
+    {
+        #region Methods
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        #endregion
+    }
+}
