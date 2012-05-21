d511753e14bc9274691dd633de73cf5a3f4599a0
gep13 <gep13@gep13.co.uk>
2012-05-21 19:51:47 +0100

    Added the blank project for the second demo.

---
+++ src/Demo 2 - StyleCop As Part of the Build/Demo 2 - StyleCop As Part of the Build/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Demo_2___StyleCop_As_Part_of_the_Build
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
