cf185d55cbefd0b4ca62e2a261c6694dc65f1a98
gep13 <gep13@gep13.co.uk>
2012-05-22 07:25:41 +0100

    Added a sample project for showing the Custom Rules in Action

---
+++ src/Demo 5a - Debug and Test Custom StyleCop Rule/Demo 5a - Debug and Test Custom StyleCop Rule/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Demo_5a___Debug_and_Test_Custom_StyleCop_Rule
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
